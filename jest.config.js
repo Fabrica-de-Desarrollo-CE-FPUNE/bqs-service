c41124d044f0738f06744b3981a84f1968d1e125
elialm7 <metall65eli@gmail.com>
2024-07-03 20:14:17 -0400

    configuracin para testing con jest

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: ['<rootDir>/tests/**/*.test.ts', '<rootDir>/tests/**/*.test.tsx'],
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    globals: {
+      'ts-jest': {
+        tsconfig: 'tsconfig.json'
+      }
+    },
+    transform: {
+      '^.+\\.(ts|tsx)$': 'ts-jest'
+    },
+    moduleNameMapper: {
+      '^@src/(.*)$': '<rootDir>/src/$1'
+    },
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coverageReporters: ['text', 'lcov'],
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js']
+  };
+  
