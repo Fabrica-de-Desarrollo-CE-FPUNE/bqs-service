f1c33f1bd7fe2baac10a1ff3f4a0e17aa0fd3d02
eli _ <metall65eli@gmail.com>
2024-07-13 23:47:34 -0400

    feat(bot/scraper): definicion de unos primeros metodos para las posibles implementacion del scraper

--- src/bot/scraper/ConsultorScraperInterfaces.ts
+++ src/bot/scraper/ConsultorScraperInterfaces.ts
@@ -1,3 +1,5 @@
 export interface IConsultorScraper {
-    
+    init():Promise<void>;
+    logIn(cedula:string, contrasenia:string):Promise<void>;
+    extraerPaginaActual():Promise<string|null>;
 }
