1ea702f71ffcd847d77029840ec86f0b1fac8c7a
eli _ <metall65eli@gmail.com>
2024-07-16 00:20:51 -0400

    chore(tests): primer test y primera caso de uso la clase ConsultorScraperPuppeteer

---
+++ tests/ConsultorScrapperPuppeteer.test.ts
@@ -0,0 +1,22 @@
+import { Alumno_credencial_login } from "./../src/types/ConsultorInfoTipos/ConsultorEstudianteCredenciales";
+import { ConsultorScraperPuppeteer } from "./../src/bot/scraper/ConsultorScraperImp";
+import puppeteer, { Browser } from "puppeteer";
+
+describe("Test ConsultorScraperPuppeteer", () => {
+  it("deberia inicializar la clase correctamente", async () => {
+    let credenciales: Alumno_credencial_login = {
+      cedula: "",
+      contrasenia: "",
+    };
+    jest.spyOn(puppeteer, "launch").mockResolvedValueOnce({
+      newPage: jest.fn().mockResolvedValueOnce({}),
+      close: jest.fn().mockResolvedValueOnce({}),
+    } as unknown as Browser);
+
+    let csp: ConsultorScraperPuppeteer = new ConsultorScraperPuppeteer(
+      credenciales
+    );
+    await expect(csp.init()).resolves.not.toThrow();
+    expect(csp).toBeDefined();
+  });
+});
