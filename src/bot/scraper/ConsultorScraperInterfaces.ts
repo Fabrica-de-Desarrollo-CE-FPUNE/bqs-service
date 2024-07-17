77a32e7f57b062d3cba3310b0711abeafae1aecb
eli _ <metall65eli@gmail.com>
2024-07-17 19:46:36 -0400

    feat(bot): implementacion del login del bot terminado

--- src/bot/scraper/ConsultorScraperInterfaces.ts
+++ src/bot/scraper/ConsultorScraperInterfaces.ts
@@ -10,7 +10,7 @@ export interface IConsultorScraperBase {
     /**
      * Inicializa el scraper aplicando las configuraciones necesarias y lanzando el navegador. 
      * @returns {Promise<void>} una promesa que se resuelve cuando la inicializacion esta completa. 
-     * @throws {ErrorDeInicializacion} Este error se lanza cuando la inicializacion falla.
+     * @throws {ScraperError} Este error se lanza cuando la inicializacion falla.
      * @since v1.0.0
      */
     init():Promise<void>; 
@@ -19,7 +19,7 @@ export interface IConsultorScraperBase {
      * Extrae el contenido de una página web.
      * @returns {Promise<string | null>} Una promesa que se resuelve con el contenido HTML de la página,
      * o null si la extracción falla o no se encuentra contenido.
-     * @throws {ErrorDeExtraccionContenido} Si la extracción de HTML falla.
+     * @throws {ScraperError} Si la extracción de HTML falla.
      * @since v1.0.0
      */
     extractPageContent():Promise<string|null>;
@@ -41,8 +41,15 @@ export interface IConsultorScraperLogin{
      /**
      * Inicia sesión en una aplicación web usando las credenciales proporcionadas.
      * @returns {Promise<void>} Una promesa que se resuelve cuando el proceso de inicio de sesión está completo.
-     * @throws {ErrorDeLogin} Si el inicio de sesión falla.
+     * @throws {ScraperError} Si el inicio de sesión falla.
      * @since v1.0.0
      */
     logIn():Promise<void>;
+      /**
+     * Va a la pagina para hacer login de la pagina del consultor.
+     * @returns {Promise<void>} Una promesa que se resuelve cuando el proceso de inicio de sesión está completo.
+     * @throws {ScraperError} Si el inicio de sesión falla.
+     * @since v1.0.0
+     */
+    gotoLogIn():Promise<void>;
 }
