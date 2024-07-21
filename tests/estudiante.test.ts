90ac2423a08951af7c730acbe1ba18c40cbd5941
David Delvalle Rojas <ddelvallerojas9@gmail.com>
2024-07-21 00:20:33 -0400

    test: agregar pruebas para DeveloperController con manejo de errores
    - Implementar pruebas para el endpoint /api/dev/estudiante.
    - Caso 1: Verificar respuesta exitosa sin query parameters.
    - Caso 2: Verificar respuesta de error con query falla1 y status 404 NOT_FOUND.
    - Caso 3: Verificar respuesta de error con query falla2 y status 503 SERVICE_UNAVAILABLE.
    - Incluir middleware de manejo de errores para capturar y manejar errores correctamente en las pruebas.
    
    Esto asegura que el endpoint maneja las solicitudes y errores de manera esperada y proporciona cobertura de pruebas.

--- tests/estudiante.test.ts
+++ tests/estudiante.test.ts
@@ -1,21 +1,51 @@
 import request from 'supertest';
-
 import express from 'express';
-
-import estudianteRouter from '../src/api/routers/estudianteRouter';
+import { StatusCodes } from 'http-status-codes';
+import developerRouter from '../src/api/routers/developerRouter';
+import { errorHandler } from '../src/api/middlewares/errorMiddleware';
 
 const app = express();
 
 app.use(express.json());
-app.use('/api', estudianteRouter);
+app.use('/api', developerRouter);
+app.use(errorHandler);
 
-describe('get /api/estudiante', ()=>{
+/**
+ * Tests para el endpoint /api/dev/estudiante
+ */
+describe('get /api/dev/estudiante', () => {
 
-    it('debe dar como resultado un simple ok', async () => {
-        const response = await request(app).get('/api/estudiante/').query({});
-        expect(response.status).toBe(200);
-        expect(response.body).toEqual({data:'Ok'});
-      });
+    /**
+     * Caso 1: Sin query, debe dar como resultado el status 200 OK y el cuerpo de la respuesta correspondiente.
+     */
+    it('caso 1: sin query, debe dar como resultado el status 200 OK.', async () => {
+        const response = await request(app).get('/api/dev/estudiante/').query({});
+        expect(response.status).toBe(StatusCodes.OK);
+        expect(response.body).toEqual(
+          {
+            nombre:'Michael',
+            apellido:'Jackson',
+            cedula:'6161000'
+          }
+        );
+    });
 
+    /**
+     * Caso 2: Con query falla1, debe emitir el status 404 NOT_FOUND.
+     */
+    it('caso 2: con query, debe fallar y emitir el status 404 NOT_FOUND.', async () => {
+      const response = await request(app).get('/api/dev/estudiante/').query({ falla1: true });
+      expect(response.status).toBe(StatusCodes.NOT_FOUND);
+      console.log(response.body);
+    });
+
+    /**
+     * Caso 3: Con query falla2, debe emitir el status 503 SERVICE_UNAVAILABLE.
+     */
+    it('caso 3: con query, debe fallar y emitir el status 503 SERVICE_UNAVAILABLE.', async () => {
+      const response = await request(app).get('/api/dev/estudiante/').query({ falla2: true });
+      expect(response.status).toBe(StatusCodes.SERVICE_UNAVAILABLE);
+      console.log(response.body);
+    });
 
 });
