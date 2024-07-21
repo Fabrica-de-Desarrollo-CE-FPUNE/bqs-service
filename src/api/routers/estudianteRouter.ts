5dda0e860876479f2edf2ecac7df2dce37e3886e
David Delvalle Rojas <ddelvallerojas9@gmail.com>
2024-07-21 00:17:20 -0400

    feat: agregar EstudianteController y estudianteRouter con separación de responsabilidades
    - Implementar el EstudianteController con el método getInfoEstudiante para manejar operaciones relacionadas con estudiantes.
    - Crear el estudianteRouter para definir la ruta de obtención de información del estudiante.
    - Separar responsabilidades entre el controlador y el enrutador.
    
    Esto mejora la estructura del código y facilita el mantenimiento y la escalabilidad de la aplicación.

--- src/api/routers/estudianteRouter.ts
+++ src/api/routers/estudianteRouter.ts
@@ -1,22 +1,13 @@
 import { Request, Response, Router } from "express";
+import { EstudianteController } from "../controllers/estudianteController";
 
 
 const estudianteRouter:Router = Router();
 
 const urlRoute:string = '/estudiante';
 
-estudianteRouter.get(urlRoute, async (req:Request, res:Response)=>{
-    console.log(req.query);
-    try {
-        // Algún proceso de la capa de servicio...
-        const resultado = {data:'Ok'};
-        // Envio de la información de la capa...
-        res.status(200).json(resultado);
-    } catch (error) {
-        // status(500) se utiliza para errores internos del servidor, sin embargo
-        // no es una buena practica, trabajaremos en un middleware de handler de errores.
-        res.status(500).json({error:'Error interno del servidor: (Causa)' + error});
-    }
-});
+const controller = new EstudianteController();
+
+estudianteRouter.get(urlRoute, controller.getInfoEstudiante);
 
 export default estudianteRouter;
