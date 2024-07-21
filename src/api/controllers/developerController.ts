f0b6d26622efa95d8f9cf503a4c7bcb7ed2f2e07
David Delvalle Rojas <ddelvallerojas9@gmail.com>
2024-07-21 00:13:28 -0400

    feat: agregar método getInfoEstudiante en DeveloperController con manejo de errores
    - Implementar el método getInfoEstudiante para obtener información del estudiante.
    - Manejar errores específicos relacionados con estudiantes utilizando EstudianteError:
      - notFoundEstudianteInfo: Lanzado si falla1 está presente en la query.
      - notServiceEstudiante: Lanzado si falla2 está presente en la query.
    - Devolver la información del estudiante en caso de éxito.

---
+++ src/api/controllers/developerController.ts
@@ -0,0 +1,54 @@
+import { Response, Request, NextFunction } from "express";
+import { StatusCodes } from "http-status-codes";
+import { info_estudiante } from "../../types/ConsultorInfoTipos/ConsultorEstudianteTipos";
+import { EstudianteError } from '../errors/EstudianteError';
+
+/**
+ * Controlador para operaciones relacionadas con desarrolladores.
+ * 
+ * @version 1.0.0
+ * @author David Delvalle Rojas
+ */
+export class DeveloperController {
+
+    /**
+     * Obtener información del estudiante.
+     * 
+     * @param {Request} req - Objeto de solicitud HTTP.
+     * @param {Response} res - Objeto de respuesta HTTP.
+     * @param {NextFunction} next - Función para pasar al siguiente middleware.
+     * 
+     * @returns {Promise<void>} - Retorna una promesa que resuelve a void.
+     * 
+     * @throws {EstudianteError.notFoundEstudianteInfo} - Error lanzado si falla1 está presente en la query.
+     * @throws {EstudianteError.notServiceEstudiante} - Error lanzado si falla2 está presente en la query.
+     * 
+     * @example
+     * // Ejemplo de llamada a este método
+     * app.get('/info-estudiante', developerController.getInfoEstudiante);
+     */
+    public getInfoEstudiante = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+        try {
+            const { falla1, falla2 } = req.query;
+
+            if (falla1) {
+                throw EstudianteError.notFoundEstudianteInfo();
+            }
+
+            if (falla2) {
+                throw EstudianteError.notServiceEstudiante();
+            }
+
+            const data: info_estudiante = {
+                nombre: 'Michael',
+                apellido: 'Jackson',
+                cedula: '6161000'
+            };
+
+            res.status(StatusCodes.OK).json(data);
+
+        } catch (error) {
+            next(error);
+        }
+    }
+}
