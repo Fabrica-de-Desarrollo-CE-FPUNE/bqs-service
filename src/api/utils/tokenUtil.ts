a39952f733120998f1e298ef5cae9e7fd5e1425e
eli _ <metall65eli@gmail.com>
2024-10-01 18:58:18 -0400

    fix

--- src/api/utils/tokenUtil.ts
+++
@@ -1,8 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-const SECRET_KEY: string = "la super contraseña que debería ser anonima y ubicada como variable de entorno"+
-" pero que siento que es muy inseguro igual y prefiero hacer que sea dinamico";
-
-export const firmarToken = (data:any)=>{
-    return  jwt.sign(data, SECRET_KEY );
-}
