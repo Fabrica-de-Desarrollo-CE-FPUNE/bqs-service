9c877b4e1b21a117312b47f8e1c1ce924a1fd182
David Delvalle <ddelvallerojas9@gmail.com>
2025-01-24 17:44:41 -0300

    feat: agregar Perfil.ts y caché de un minuto

--- src/postgre/data-source.ts
+++ src/postgre/data-source.ts
@@ -1,6 +1,6 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
-import { Usuario } from "./entity/Usuario"
+import { Perfil } from "./entity/Perfil"
 import { Materia } from "./entity/Materia"
 import { Inscripcion } from "./entity/Inscripcion"
 import { Periodo } from "./entity/Periodo"
@@ -8,11 +8,15 @@ import { Escala } from "./entity/Escala"
 import { ResultadoParcial } from "./entity/ResultadoParcial"
 import { ExamenFinal } from "./entity/ExamenFinal"
 import * as dotenv from "dotenv"
+import { Usuario } from "./entity/Usuario"
 
 dotenv.config({path:'.env'});
 
 export const AppDataSource = new DataSource({
-    cache:true,
+    cache:{
+        alwaysEnabled:true,
+        duration:60000,
+    },
     type: "postgres",
     host: process.env.HOST,
     port: Number(process.env.PORT_DB),
@@ -21,7 +25,9 @@ export const AppDataSource = new DataSource({
     database: process.env.DB,
     synchronize: true, // Borra toda la base de datos cada que arranca, cuidado con este en produccion
     logging: false,
-    entities: [Usuario, Materia, Periodo, Inscripcion, Escala, ResultadoParcial, ExamenFinal],
+    entities: [Usuario, Perfil, Materia, Periodo, Inscripcion, Escala, ResultadoParcial, ExamenFinal],
     migrations: [],
     subscribers: [],
-})
+});
+
+AppDataSource.initialize();
